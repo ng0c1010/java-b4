c5e9e46c7c4e1a68f35247f5702dcef15a17e84a
ng0c1010 <96835034+ng0c1010@users.noreply.github.com>
2023-05-22 16:59:46 +0700

    Update GiaoDich.java

--- GiaoDich.java
+++ GiaoDich.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package newpackage;
 
-/**
- *
- * @author ADMIN
- */
 class GiaoDich {
     protected String maGd;
     protected String ngayGd;
@@ -25,3 +16,4 @@ class GiaoDich {
         return dienTich * donGia;
     }
 }
+
